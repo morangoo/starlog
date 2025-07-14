a77ec5adb0a767b3c6e38baaaf8a11a5eceaf14d
Bruno Santos <bms.santos@outlook.pt>
2025-07-14 16:39:52 +0100

    Now compatible with Dark Mode.

---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: [
+    './components/**/*.{vue,js,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './app.vue',
+    './plugins/**/*.{js,ts}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
