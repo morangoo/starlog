0e3290af689b0e2d1712b1411b35d9e21a94d1f3
Bruno Santos <bms.santos@outlook.pt>
2025-07-14 17:21:09 +0100

    We have a favicon now!

--- app.vue
+++ app.vue
@@ -4,3 +4,10 @@
     <NuxtLayout />
   </div>
 </template>
+
+<script setup>
+  useHead({
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    title: "Starlog"
+  })
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
-  css: ['~/assets/main.css']
+  css: ['~/assets/main.css'],
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
+    }
+  }
 })
